c32109b70cac7be6028f3767fcbc7e76d479eea0
Santiago Cuevas <santiagocuevas77@gmail.com>
2024-04-19 20:16:23 -0600

    add checkRows

--- script.js
+++ script.js
@@ -31,7 +31,23 @@ const createGame = () => {
     activePlayer = currentPlayer === TileState.O ? TileState.X : TileState.O;
   };
 
-  const startGame = () => {};
+  const startGame = () => {
+    // while (!winner)
+  };
 
-  // Game loop: Assign active player then active player chooses tile. Board state is updated with tile. Evaluate board state (check for winner). If winner, exit game loop and enter on win flow with active player. If !winner switch active player.
+  const evaluateBoard = () => {};
+
+  const checkRows = (board, activePlayer) => {
+    for (let i = 0; i < board.length; i++) {
+      const activePlayerWon = board[i].every((item) => {
+        return item === activePlayer;
+      });
+
+      if (activePlayerWon) {
+        return true;
+      }
+    }
+    return false;
+  };
 };
+// Game loop: Assign active player then active player chooses tile. Board state is updated with tile. Evaluate board state (check for winner). If winner, exit game loop and enter on win flow with active player. If !winner switch active player.
